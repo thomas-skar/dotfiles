488c1aa14af169612a894cf1acc0fab931618fcf
Thomas Skar <thomas.skar@intility.no>
2026-08-13 12:46:06 +0200

    chore(system.nix): update nix.conf settings

--- system.nix
+++ system.nix
@@ -1,11 +1,30 @@
 { pkgs, ... }:
 {
   config = {
+    # system-manager.allowAnyDistro = false;
+    # system-manager.linkCurrentSystem = true;
+
     nixpkgs.hostPlatform = "x86_64-linux";
     nixpkgs.config.allowUnfree = true;
 
     nix.enable = true;
-    nix.settings.experimental-features = "nix-command flakes";
+
+    # /etc/nix/nix.conf
+    nix.settings = {
+      experimental-features = "nix-command flakes";
+      trusted-users = [ "thomas" ];
+      auto-optimise-store = false;
+      trusted-substituters = [
+        "https://cache.nixos.org/"
+        "https://cache.numtide.com"
+        "https://noctalia.cachix.org"
+      ];
+      trusted-public-keys = [
+        "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+        "niks3.numtide.com-1:DTx8wZduET09hRmMtKdQDxNNthLQETkc/yaX7M4qK0g="
+        "noctalia.cachix.org-1:pCOR47nnMEo5thcxNDtzWpOxNFQsBRglJzxWPp3dkU4="
+      ];
+    };
 
     environment.systemPackages = with pkgs; [
       curl
@@ -14,10 +33,14 @@
     services.userborn.enable = true;
 
     users.users.thomas = {
+      enable = true;
       isNormalUser = true;
+      createHome = true;
       uid = 1000;
       group = "thomas";
+      extraGroups = [ ];
       home = "/home/thomas";
+      homeMode = "700";
     };
     users.groups.thomas.gid = 1000;
 
