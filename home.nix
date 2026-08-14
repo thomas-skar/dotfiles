6bd59373e3c1f4bf3ac017096a54044404154129
Thomas Skar <thomas.skar@intility.no>
2026-08-14 12:36:25 +0200

    chore(home.nix): remove run-or-raise shell script

--- home.nix
+++ home.nix
@@ -47,27 +47,6 @@ in
   home.sessionPath = [
     "$HOME/.local/bin"
   ];
-  home.file.".local/bin/run-or-raise" = {
-    enable = true;
-    executable = true;
-    text = ''
-      #!/bin/sh
-
-      if test -z "$1"; then
-          echo "Usage: run-or-raise app_id [executable]"
-          exit 1
-      fi
-
-      app_id=$1
-      executable=$2
-      test -z "$executable" && executable=$app_id
-
-      if ! wlrctl window focus "$app_id"; then
-          $executable &
-          disown
-      fi
-    '';
-  };
 
   programs.home-manager.enable = true;
 
