3333a677cef94de78579c8fdf6e2cf9c5e1ef5d6
Thomas Skar <thomas.skar@intility.no>
2026-08-18 16:21:38 +0200

    fix(flake.nix): make system-manager follow nixpkgs; update the flake.lock file

--- flake.nix
+++ flake.nix
@@ -10,7 +10,7 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
 
     system-manager.url = "github:numtide/system-manager";
-    # system-manager.inputs.nixpkgs.follows = "nixpkgs"; # TODO: broken atm
+    system-manager.inputs.nixpkgs.follows = "nixpkgs";
 
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
