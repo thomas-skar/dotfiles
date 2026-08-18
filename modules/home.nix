f4df89b9461d76c586fd3be7cd329e62eb810f81
Thomas Skar <thomas.skar@intility.no>
2026-08-18 12:33:40 +0200

    feat(home.nix): enable kanshi service to handle display placements

--- modules/home.nix
+++ modules/home.nix
@@ -178,6 +178,33 @@ in
 
   services.podman.enable = true;
 
+  services.kanshi = {
+    enable = true;
+    profiles = {
+      "undocked" = {
+        outputs = [
+          { criteria = "eDP-1"; }
+        ];
+      };
+      "docked" = {
+        outputs = [
+          {
+            criteria = "eDP-1";
+            position = "1280,1440";
+          }
+          {
+            criteria = "DVI-I-1";
+            position = "2560,0";
+          }
+          {
+            criteria = "DVI-I-2";
+            position = "0,0";
+          }
+        ];
+      };
+    };
+  };
+
   # theming
   gtk = {
     enable = true;
