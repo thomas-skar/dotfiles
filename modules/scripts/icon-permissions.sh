4fb7c979823d24a44b7833fda77fbad233515524
Thomas Skar <thomas.skar@intility.no>
2026-08-19 09:27:44 +0200

    chore: update the icon permissions script to also fix the 'pixmaps' directory

--- modules/scripts/icon-permissions.sh
+++ modules/scripts/icon-permissions.sh
@@ -6,6 +6,7 @@ if [[ "$1" == "" ]]; then
 fi
 
 chmod -R 755 "/etc/profiles/per-user/$1/share/icons"
+chmod -R 755 "/etc/profiles/per-user/$1/share/pixmaps"
 chmod -R 755 "/home/$1/.local/share/icons"
 
 # TODO ?
