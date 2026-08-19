6b55dee796490a107a6f8e38b556c313acc8ac34
Thomas Skar <thomas.skar@intility.no>
2026-08-19 14:36:12 +0200

    fix(system): hard-code the login shell path so it doesn't get reset @ every build

--- modules/system/default.nix
+++ modules/system/default.nix
@@ -42,12 +42,14 @@ in
   users.users.thomas = {
     enable = true;
     isNormalUser = true;
-    createHome = true;
+    createHome = false;
     uid = 1000;
     group = "thomas";
     extraGroups = [ ];
     home = "/home/thomas";
     homeMode = "700";
+    shell = "/etc/profiles/per-user/thomas/bin/fish";
+    useDefaultShell = true;
   };
   users.groups.thomas.gid = 1000;
 
