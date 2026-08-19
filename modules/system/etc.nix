b8f90703c65a17fc0dc9a9313a6f8f0c3b569440
Thomas Skar <thomas.skar@intility.no>
2026-08-19 13:20:33 +0200

    chore(system): remove the noctalia pam file (it didn't do anything)

--- modules/system/etc.nix
+++ modules/system/etc.nix
@@ -1,4 +1,5 @@
 {
+  # TODO: not sure if this is actually needed
   environment.etc."locale.conf" = {
     enable = true;
     user = "root";
@@ -7,31 +8,6 @@
     replaceExisting = true;
     text = ''
       LANG=nb_NO.UTF-8
-    '';
-  };
-
-  environment.etc."pam.d/noctalia" = {
-    enable = true;
-    user = "root";
-    group = "root";
-    mode = "0644";
-    text = ''
-      #%PAM-1.0
-      auth    requisite       pam_nologin.so
-      auth    required        pam_succeed_if.so user != root quiet_success
-      @include common-auth
-      auth    optional        pam_gnome_keyring.so
-      @include common-account
-      session [success=ok ignore=ignore module_unknown=ignore default=bad]        pam_selinux.so close
-      session required        pam_loginuid.so
-      session [success=ok ignore=ignore module_unknown=ignore default=bad]        pam_selinux.so open
-      session optional        pam_keyinit.so force revoke
-      session required        pam_limits.so
-      session required        pam_env.so readenv=1
-      session required        pam_env.so readenv=1 user_readenv=1 envfile=/etc/default/locale
-      @include common-session
-      session optional        pam_gnome_keyring.so auto_start
-      @include common-password
     '';
   };
 
