e4a796516a9b1b6dd55512406b491c63fd04bd91
Thomas Skar <thomas.skar@intility.no>
2026-08-20 16:20:27 +0200

    chore: fix the ghr.fish script

--- scripts/ghr.fish
+++ scripts/ghr.fish
@@ -1,6 +1,6 @@
 
 # github repo
-function ghr
+function ghr --argument-names owner topic
     if ! command -v gh > /dev/null
         echo "gh is not installed"
         return 1
@@ -11,6 +11,19 @@ function ghr
         return 1
     end
 
-    # TODO: check gh login status ?
-    # TODO: list repos
+    if test (gh auth status --json hosts --jq '.hosts.["github.com"][0].state') != "success"
+        echo "you're not logged in"
+        return 1
+    end
+
+    set -f repos (gh repo list "$owner" --topic "$topic" --json url --jq '.[].url' --limit 100)
+
+    set -f url (for url in $repos; echo $url; end | fzf)
+
+    if test -n "$url"
+        open "$url"
+        return 0
+    end
+
+    return 1
 end
