e0d8e0ef50b5f71edaf7f590cad84e9c2d05697a
Thomas Skar <thomas.skar@intility.no>
2026-08-21 15:40:30 +0200

    chore: tweak the apparmor script

--- scripts/apparmor.sh
+++ scripts/apparmor.sh
@@ -1,11 +1,12 @@
+
 # script to register custom nix apparmor profiles
 
 nix_apparmor_profiles=$(find /etc/apparmor.d/ -type l -name "nix-*")
 if (( ${#nix_apparmor_profiles[@]} > 0 )); then
     for apparmor_profile in "${nix_apparmor_profiles[@]}"; do
         if [ "$apparmor_profile" != "" ]; then
-            echo "$apparmor_profile"
-            /usr/sbin/apparmor_parser --add "$apparmor_profile" || /usr/sbin/apparmor_parser --replace "$apparmor_profile" || echo "could't add or replace profile"
+            echo "registering $apparmor_profile ..."
+            /usr/sbin/apparmor_parser --add "$apparmor_profile" || /usr/sbin/apparmor_parser --replace "$apparmor_profile" || echo "could't add or replace $apparmor_profile"
         fi
     done
 fi
