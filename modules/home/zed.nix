5c199b9c9231cd26bdcde4b4b42cd1b0a61d2cd2
Thomas Skar <thomas.skar@intility.no>
2026-08-21 11:11:54 +0200

    feat(home): add zed settings

--- modules/home/zed.nix
+++ modules/home/zed.nix
@@ -1,4 +1,117 @@
 {
-  # TODO: settings
-  programs.zed-editor.enable = true;
+  programs.zed-editor = {
+    enable = true;
+    installRemoteServer = false;
+    defaultEditor = true;
+
+    extensions = [
+      "nix"
+      "just"
+      "monokai-pro-ce"
+      "html"
+      "toml"
+      "xml"
+      "fish"
+    ];
+
+    mutableUserSettings = true;
+    userSettings = {
+      theme = "Monokai Pro (CE)";
+      buffer_font_family = "JetBrains Mono";
+      buffer_font_size = 15;
+      ui_font_size = 16;
+      base_keymap = "VSCode";
+      colorize_brackets = true;
+      vim_mode = false;
+      indent_guides.enabled = true;
+      ensure_final_newline_on_save = true;
+      remove_trailing_whitespace_on_save = true;
+      format_on_save = "on";
+      formatter = "auto";
+      close_on_file_delete = true;
+      telemetry = {
+        diagnostics = false;
+        metrics = false;
+        anthropic_retention = false;
+      };
+      window_decorations = "client";
+      which_key.enabled = true;
+      autosave = "on_focus_change";
+      auto_update = false;
+      session.trust_all_worktrees = true;
+
+      tabs = {
+        show_close_button = "always";
+        file_icons = true;
+        show_branch_status_icon = true;
+      };
+      status_bar = {
+        show_active_file = true;
+        line_endings_button = true;
+      };
+      gutter = {
+        breakpoints = false;
+      };
+      project_panel = {
+        git_status_indicator = true;
+        diagnostic_badges = true;
+        bold_folder_labels = true;
+        indent_size = 12;
+        button = true;
+        dock = "left";
+      };
+
+      semantic_tokens = "combined";
+      code_lens = "menu";
+      prettier.allowed = true;
+      enable_language_server = true;
+      inlay_hints = {
+        enabled = true;
+        show_background = true;
+      };
+      languages = {
+        "XML" = {
+          tab_size = 2;
+        };
+        "Nix" = {
+          language_servers = [
+            "nixd"
+            "nil"
+          ];
+          formatter.external = {
+            command = "nixfmt";
+            arguments = [
+              "--quiet"
+              "--"
+            ];
+          };
+        };
+      };
+      lsp = {
+        "nixd" = { };
+        "nil" = { };
+      };
+      edit_predictions = {
+        allow_data_collection = "no";
+      };
+    };
+
+    mutableUserKeymaps = true;
+    userKeymaps = [
+      {
+        context = "Editor";
+        bindings = {
+          alt-left = "editor::MoveToPreviousWordStart";
+          alt-right = "editor::MoveToNextWordEnd";
+          super-left = "editor::MoveToBeginningOfLine";
+          super-right = "editor::MoveToEndOfLine";
+          super-up = "editor::MoveToBeginning";
+          super-down = "editor::MoveToEnd";
+          alt-backspace = "editor::DeleteToPreviousWordStart";
+          super-backspace = "editor::DeleteToBeginningOfLine";
+          shift-super-backspace = "editor::DeleteToEndOfLine";
+        };
+      }
+    ];
+  };
 }
