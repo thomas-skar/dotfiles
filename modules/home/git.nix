2cd8d4465dc9e1bad1eef0a5bb96fd6a28b3421b
Thomas Skar <thomas.skar@intility.no>
2026-08-24 11:10:04 +0200

    chore(home): add lazygit settings

--- modules/home/git.nix
+++ modules/home/git.nix
@@ -8,7 +8,21 @@
     };
   };
 
-  programs.lazygit.enable = true;
+  programs.lazygit = {
+    enable = true;
+    enableFishIntegration = true;
+    settings = {
+      gui.language = "en";
+      gui.showRandomTip = false;
+      gui.nerdFontsVersion = "3";
+      git.autoFetch = false;
+      update.method = "never";
+      confirmOnQuit = false;
+      quitOnTopLevelReturn = true;
+      disableStartupPopups = true;
+      notARepository = "quit";
+    };
+  };
 
   programs.jujutsu.enable = false;
 }
