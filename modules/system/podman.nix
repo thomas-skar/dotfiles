cee6e4caee47dada78fbeeb81f16fbed74e584c5
Thomas Skar <thomas.skar@intility.no>
2026-08-26 09:41:35 +0200

    chore(system): tweak podman module

--- modules/system/podman.nix
+++ modules/system/podman.nix
@@ -5,8 +5,15 @@
   ];
 
   # TODO: add systemd service to fix newuidmap/ newgidmap permissions?
+  # TODO: I did something manually and now it kinda words...
 
-  home-manager.users."${username}".services.podman = {
-    enable = true;
+  home-manager.users."${username}" = {
+    home.packages = [
+      pkgs.shadow
+    ];
+
+    services.podman = {
+      enable = true;
+    };
   };
 }
