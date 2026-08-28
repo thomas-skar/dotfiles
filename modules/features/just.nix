baff2555a00dcac88b153783dc12ba9f5fa0c174
Thomas Skar <thomas.skar@intility.no>
2026-08-28 12:55:44 +0200

    chore(just): remove the switch recipe in the global justfile

--- modules/features/just.nix
+++ modules/features/just.nix
@@ -4,14 +4,10 @@
       pkgs.just
     ];
 
-    # TODO: environment variable: path >>> flake.nix ?
     home.file.".justfile".text = ''
       [private]
       default:
         @just --list --global-justfile
-
-      switch:
-        nix run 'github:numtide/system-manager' --extra-experimental-features "nix-command flakes" --accept-flake-config -- switch --sudo --flake /home/thomas/code/dotfiles/flake.nix#systemConfigs.default
     '';
   };
 }
