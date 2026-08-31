1b06b0ce313ccb2eea87abd8bac125a65216b228
Thomas Skar <thomas.skar@intility.no>
2026-08-31 10:16:40 +0200

    chore(noctalia): update palette

--- modules/features/noctalia/default.nix
+++ modules/features/noctalia/default.nix
@@ -1,7 +1,17 @@
 { inputs, ... }:
 {
-  flake-file.inputs.noctalia.url = "github:noctalia-dev/noctalia/cachix";
-  # flake-file.inputs.noctalia.inputs.nixpkgs.follows = "nixpkgs";
+  flake-file = {
+    inputs.noctalia = {
+      url = "github:noctalia-dev/noctalia";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    nixConfig = {
+      extra-substituters = [ "https://noctalia.cachix.org" ];
+      extra-trusted-public-keys = [
+        "noctalia.cachix.org-1:pCOR47nnMEo5thcxNDtzWpOxNFQsBRglJzxWPp3dkU4="
+      ];
+    };
+  };
 
   flake.homeModules.noctalia = {
     imports = [ inputs.noctalia.homeModules.default ];
@@ -16,7 +26,6 @@
           enable_sounds = true;
           enable_overdrive = true;
         };
-
         bar.default = {
           margin_ends = 0;
           widget_spacing = 14;
@@ -41,11 +50,9 @@
             "session"
           ];
         };
-
         control_center = {
           sidebar = "full";
           sidebar_section = "none";
-
           calendar = {
             show_events_card = true;
             show_week_numbers = true;
@@ -53,7 +60,6 @@
         };
 
         desktop_widgets.enabled = false;
-
         dock = {
           enabled = true;
           background_opacity = 0.75;
@@ -63,7 +69,6 @@
           reserve_space = false;
           show_dots = true;
         };
-
         idle = {
           behavior_order = [
             "lock"
@@ -88,33 +93,29 @@
             };
           };
         };
-
         location.address = "Oslo, Norway";
-
-        lockscreen.allow_empty_password = true;
-        lockscreen.monitors = [ "eDP-1" ];
+        lockscreen = {
+          allow_empty_password = true;
+          monitors = [ "eDP-1" ];
+        };
         lockscreen_widgets.enabled = false;
-
-        # notification.monitors = [ "eDP-1" ];
-
         osd.monitors = [ "eDP-1" ];
-
-        plugins.enabled = [ ];
-        plugins.source = [
-          {
-            name = "official";
-            enabled = false;
-          }
-          {
-            name = "community";
-            enabled = false;
-          }
-        ];
-
+        plugins = {
+          enabled = [ ];
+          source = [
+            {
+              name = "official";
+              enabled = false;
+            }
+            {
+              name = "community";
+              enabled = false;
+            }
+          ];
+        };
         shell = {
           polkit_agent = true;
           launch_apps_as_systemd_services = false;
-
           launcher = {
             categories = false;
             compact = true;
@@ -123,55 +124,43 @@
             show_icons = false;
             providers.calculator.global = false;
           };
-
           panel = {
             open_near_click_session = true;
             open_near_click_control_center = true;
           };
         };
-
         theme = {
           mode = "dark";
           source = "custom";
           custom_palette = "custom";
-
           templates = {
             enable_builtin_templates = true;
             enable_community_templates = false;
             builtin_ids = [
               "gtk3"
               "gtk4"
-              "kcolorscheme"
               "labwc"
               "qt"
             ];
           };
         };
-
         wallpaper.directory = "/usr/share/backgrounds";
-
         weather.enabled = true;
-
         widget = {
           active_window.interactive = false;
-
           date = {
             anchor = true;
             format = "{:%a %d %b - %H:%M}";
           };
-
           network = {
             show_label = false;
             vpn_status = "both";
           };
-
           spacer_start.type = "spacer";
-
           spacer_end = {
             type = "spacer";
             length = 33;
           };
-
           sysmon = {
             show_value = false;
             visualization = "none";
