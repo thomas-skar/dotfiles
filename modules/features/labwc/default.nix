cbaa962660574934707a0966099d53d875348855
Thomas Skar <thomas.skar@intility.no>
2026-09-04 10:08:26 +0200

    chore(labwc): don't run noctalia @ startup; add more environment variables; add more menu items; add keybind to open rofi; tweak pointer speed

--- modules/features/labwc/default.nix
+++ modules/features/labwc/default.nix
@@ -13,14 +13,13 @@
     wayland.windowManager.labwc = {
       enable = true;
       package = pkgs.labwc;
-      autostart = [
-        "/opt/intility/idn/Intility.DesktopNotifications.Client.Linux &"
-        "noctalia -d"
-      ];
+      autostart = [ "/opt/intility/idn/Intility.DesktopNotifications.Client.Linux" ];
       environment = [
         "XKB_DEFAULT_LAYOUT=no"
         "XDG_CURRENT_DESKTOP=labwc:wlroots"
+        "XDG_SESSION_TYPE=wayland"
         "XCURSOR_THEME=Adwaita"
+        "QA_QPA_PLATFORM=wayland"
       ];
       systemd.enable = true;
     };
