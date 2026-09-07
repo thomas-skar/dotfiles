b06d2ace38987f9b265cd4e6f4bc3cd85af68293
Thomas Skar <thomas.skar@intility.no>
2026-09-07 16:57:54 +0200

    chore(neovim): continue work on init.lua

--- modules/features/neovim/init.lua
+++ modules/features/neovim/init.lua
@@ -1,173 +1,121 @@
--- 1. vim options
+--[[ options ]]
 do
-  if vim.loader then vim.loader.enable() end
+  require('vim._core.ui2').enable {
+    enable = true,
+  }
+
+  vim.loader.enable()
 
   vim.g.mapleader = ' '
   vim.g.maplocalleader = ' '
   vim.g.have_nerd_font = true
-  vim.o.number = true -- turn on line numbers
-  vim.o.relativenumber = false -- turn off relative line numbers
-  vim.o.mouse = 'a' -- enable mouse mode
-  vim.o.showmode = false -- handled by statusline
-  vim.o.breakindent = true
-  vim.o.ignorecase = true
-  vim.o.smartcase = true
-  vim.o.signcolumn = 'yes'
-  vim.o.updatetime = 250
-  vim.o.timeoutlen = 300
-  vim.o.splitright = true
-  vim.o.splitbelow = true
-  vim.o.cursorline = true
-  vim.o.scrolloff = 10
-  vim.o.confirm = true
 
+  -- disable netrw
   vim.g.loaded_netrw = 1
   vim.g.loaded_netrwPlugin = 1
+
+  -- providers?
+  vim.g.loaded_node_provider = 0
+  vim.g.loaded_python3_provider = 0
+  vim.g.loaded_perl_provider = 0
+  vim.g.loaded_ruby_provider = 0
+
+  -- display
+  vim.opt.termguicolors = true
+  vim.opt.number = true
+  vim.opt.relativenumber = false
+  vim.opt.showmode = false
+  vim.opt.signcolumn = 'yes'
+  vim.opt.cursorline = true
+  vim.opt.winborder = 'rounded'
+  vim.opt.pumborder = 'rounded'
+  vim.opt.laststatus = 3
+
+  -- search
+  vim.opt.ignorecase = true
+  vim.opt.smartcase = true
+
+  -- splits
+  vim.opt.splitright = true
+  vim.opt.splitbelow = true
+
+  vim.opt.mouse = 'a'
 end
 
--- 2. keymaps >:)
+--[[ keymaps ]]
 do
-  -- clear search highlight with <Esc>
   vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
 
-  -- search with <Ctrl-F>
-  vim.keymap.set({ 'n', 'v' }, '<C-f>', '/')
-
-  -- open the telescope file picket with <Ctrl-P>
-  vim.keymap.set({ 'n', 'v', 'i' }, '<C-p>', '<CMD>Telescope find_files<CR>')
-
-  -- open the telescope "find in files" with <Shift-Ctrl-F>
-  vim.keymap.set({ 'n', 'v', 'i' }, '<S-C-f>', '<CMD>Telescope grep_string<CR>')
-
-  -- open oil with <Ctrl-O>
-  vim.keymap.set({ 'n', 'v', 'i' }, '<C-o>', '<CMD>Oil --float<CR>')
+  vim.keymap.set('n', '<C-e>', '<cmd>NvimTreeToggle<CR>')
 end
 
--- 3. plugin pre-reqs
+--[[ plugins: colorscheme ]]
 do
-  local function run_build(name, cmd, cwd)
-    local result = vim.system(cmd, { cwd = cwd }):wait()
-    if result.code ~= 0 then
-      local stderr = result.stderr or ''
-      local stdout = result.stdout or ''
-      local output = stderr ~= '' and stderr or stdout
-      if output == '' then output = 'No output from build command.' end
-      vim.notify(('Build failed for %s:\n%s'):format(name, output), vim.log.levels.ERROR)
-    end
-  end
-
-  vim.api.nvim_create_autocmd('PackChanged', {
-    callback = function(ev)
-      local name = ev.data.spec.name
-      local kind = ev.data.kind
-      if kind ~= 'install' and kind ~= 'update' then return end
-
-      if name == 'telescope-fzf-native.nvim' and vim.fn.executable 'make' == 1 then
-        run_build(name, { 'make' }, ev.data.path)
-        return
-      end
-    end,
-  })
-end
-
--- 4. plugins
-do
-  -- [[ colorscheme ]]
   vim.pack.add { 'https://github.com/loctvl842/monokai-pro.nvim' }
   require('monokai-pro').setup {
     filter = 'pro',
     devicons = true,
   }
   vim.cmd.colorscheme 'monokai-pro'
-
-  -- [[ mini ]]
-  vim.pack.add { 'https://github.com/nvim-mini/mini.nvim' }
-
-  -- [[ mini icons ]]
-  if vim.g.have_nerd_font then require('mini.icons').setup() end
-
-  -- [[ mini statusline ]]
-  require('mini.statusline').setup { use_icons = vim.g.have_nerd_font }
-
-  -- [[ mini tabline ]]
-  require('mini.tabline').setup()
-
-  -- [[ mini cmdline ]]
-  require('mini.cmdline').setup {
-    autocomplete = {
-      enable = true,
-      map_arrows = true,
-    },
-    autocorrect = {
-      enable = true,
-    },
-    autopeek = {
-      enable = true,
-    },
-  }
-
-  -- [[ mini notify ]]
-  require('mini.notify').setup {
-    lsp_progress = {
-      enable = true,
-    },
-  }
-
-  -- [[ mini indentscope ]]
-  require('mini.indentscope').setup {
-    draw = {
-      animation = require('mini.indentscope').gen_animation.none(),
-    },
-  }
-
-  -- [[ mini cursorword ]]
-  require('mini.cursorword').setup()
-
-  -- [[ todo comments]]
-  vim.pack.add { 'https://github.com/folke/todo-comments.nvim' }
-  require('todo-comments').setup { signs = false }
-
-  -- [[ oil  ]]
-  vim.pack.add { 'https://github.com/stevearc/oil.nvim' }
-  require('oil').setup {
-    default_file_explorer = true,
-    delete_to_trash = true,
-    view_options = {
-      show_hidden = true,
-    },
-    float = {
-      border = 'rounded',
-    },
-  }
 end
 
--- 5. telescope
+--[[ plugins: mini ]]
 do
-  local telescope_plugins = {
-    'https://github.com/nvim-lua/plenary.nvim',
-    'https://github.com/nvim-telescope/telescope.nvim',
-  }
+  vim.pack.add { 'https://github.com/nvim-mini/mini.nvim' }
 
-  if vim.fn.executable 'make' == 1 then table.insert(telescope_plugins, 'https://github.com/nvim-telescope/telescope-fzf-native.nvim') end
+  if vim.g.have_nerd_font then
+    require('mini.icons').setup { style = 'ascii' }
+    vim.cmd ':lua MiniIcons.mock_nvim_web_devicons()'
+  end
+  require('mini.statusline').setup { use_icons = vim.g.have_nerd_font }
+  require('mini.tabline').setup {}
+  require('mini.cmdline').setup {}
+  require('mini.notify').setup {}
+  require('mini.indentscope').setup {}
+  require('mini.cursorword').setup {}
+end
 
-  vim.pack.add(telescope_plugins)
+--[[ plugins: nvim-tree ]]
+do
+  vim.pack.add { 'https://github.com/nvim-tree/nvim-tree.lua' }
+  require('nvim-tree').setup {}
+end
 
-  require('telescope').setup {
-    defaults = {
-      mappings = {
-        i = {
-          ['<esc>'] = require('telescope.actions').close,
+--[[ plugins: telescope ]]
+do
+end
+
+--[[ lsp ]]
+do
+  local capabilities = vim.lsp.protocol.make_client_capabilities()
+  vim.lsp.config('*', {
+    capabilities = capabilities,
+  })
+
+  vim.lsp.config('lua-language-server', {
+    settings = {
+      Lua = {
+        diagnostics = {
+          globals = { 'vim' },
+        },
+        telemetry = {
+          enable = false,
         },
       },
     },
-    extensions = {
-      fzf = {
-        fuzzy = true,
-        override_generic_sorter = true,
-        override_file_sorter = true,
-        case_mode = 'smart_case',
-      },
-    },
-  }
-  pcall(require('telescope').load_extension, 'fzf')
+  })
+
+  vim.lsp.enable { 'lua_ls' }
+end
+
+--[[ formatting ]]
+do
+end
+
+--[[ autocomplete ]]
+do
+end
+
+--[[ treesitter ]]
+do
 end
