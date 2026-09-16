5719d95e234ff2ac6f58b7308a865d9c2405db0f
Thomas Skar <thomas.skar@intility.no>
2026-09-16 16:54:44 +0200

    chore: add bruno module

---
+++ modules/features/bruno.nix
@@ -0,0 +1,14 @@
+{ self, ... }:
+{
+  flake.nixosModules.bruno = {
+    home-manager.sharedModules = [ self.homeModules.bruno ];
+  };
+
+  flake.homeModules.bruno = { pkgs, ... }: {
+    home.packages = [ pkgs.bruno ];
+
+    home.sessionVariables = {
+      TMPDIR = "$HOME/tmp";
+    };
+  };
+}
